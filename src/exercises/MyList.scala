013e2d80eedce9ce64de356e8d75400cecb212ae
Hugo Brito <hubr@itu.dk>
2019-10-08 20:44:49 +0200

    finished generics and anonymous classes

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -4,14 +4,7 @@
 
 package exercises
 
-abstract class MyList {
-	def head: Int
-	def tail: MyList
-	def isEmpty: Boolean
-	def add(elem: Int): MyList
-	// polymorphic call
-	def printElements: String
-	override def toString: String = "[" + printElements + "]"
+abstract class MyList[+A] {
 
 	/*
 		head: Int = first element of the list
@@ -21,21 +14,29 @@ abstract class MyList {
 		override toString => a string representation of the list
 	 */
 
+	def head: A
+	def tail: MyList[A]
+	def isEmpty: Boolean
+	def add[B >: A](elem: B): MyList[B]
+	// polymorphic call
+	def printElements: String
+	override def toString: String = "[" + printElements + "]"
+
 }
 
-object Empty extends MyList {
-	def head: Int = throw new NoSuchElementException
-	def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+	def head: Nothing = throw new NoSuchElementException
+	def tail: MyList[Nothing] = throw new NoSuchElementException
 	def isEmpty: Boolean = true
-	def add(elem: Int): MyList = new Cons(elem, Empty)
+	def add[B >: Nothing](elem: B): MyList[B] = new Cons(elem, Empty)
 	override def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-	def head: Int = h
-	def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+	def head: A = h
+	def tail: MyList[A] = t
 	def isEmpty: Boolean = false
-	def add(elem: Int): MyList = new Cons(elem, this)
+	def add[B >: A](elem: B): MyList[B] = new Cons(elem, this)
 	override def printElements: String =
 		if(t.isEmpty) "" + h
 		else h + " " + t.printElements
@@ -47,5 +48,11 @@ object ListTest extends App {
 	println(list.add(4).head)
 	println(list.isEmpty)
 	println(list.toString)
+
+	val listOfInteger: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+	val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+	println(listOfInteger.toString)
+	println(listOfStrings.toString)
 
 }
--- src/lectures/part2oop/Generics.scala
+++ src/lectures/part2oop/Generics.scala
@@ -4,6 +4,60 @@
 
 package lectures.part2oop
 
-object Generics {
+object Generics extends App {
 
+	class MyList[+A] {
+		// use the type A
+		def add[B >: A](element: B): MyList[B] = ???
+		// B is supertype of A
+		// If to a list of A (which is subtype of B), I add and object of type B, the resulting list will be a list of B
+		/*
+		 A = Cat
+		 B = Animal
+		 */
+	}
+
+	class MyMap[Key, Value]
+
+	val listOfIntegers = new MyList[Int]
+	val listOfStrings = new MyList[String]
+
+	// generic methods
+	object MyList { // companion objects cannot be type parameterized
+		def empty[A]: MyList[A] = ???
+	}
+
+	val emptyListOfIntegers = MyList.empty[Int]
+
+	// variance problem
+	class Animal
+	class Cat extends Animal
+	class Dog extends Animal
+
+	// 1. yes List[Cat] extends List[Animal] = COVARIANCE
+	class CovariantList[+A]
+	val animal: Animal = new Cat
+	val animalList: CovariantList[Animal] = new CovariantList[Cat]
+	// animalList.add(new Dog) ??? => Yes, but then instead of a List[Cat] we return a list of animals
+
+	// 2. No = INVARIANCE
+	class InvariantList[A]
+	val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+	// 3. Hell, no! CONTRAVARIANCE -- the opposite of COVARIANCE
+	class ContravariantList[-A]
+	val contravariantList: ContravariantList[Cat] = new ContravariantList[Animal]
+	// another example
+	class Trainer[-A]
+	val trainer: Trainer[Cat] = new Trainer[Animal]
+
+	//bounded types
+	class Cage[A <: Animal](animal: A) // this is an upper bound type on Animal
+	// only accepts subclasses of Animal (must be a subclass such as Cat, Dog)
+	val cage = new Cage(new Dog)
+
+	class Car
+//	val newCage = new Cage(new Car)
+
+	// expand MyList to be generic
 }
